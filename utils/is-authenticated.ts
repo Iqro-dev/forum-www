9d4db5f11c012d112ff682151972b5da93b5e65f
Iqro-dev <igor.sokolowski.code@gmail.com>
2024-01-26 07:46:24 +0100

    feat(auth): refresh token if access token expired

--- utils/is-authenticated.ts
+++ utils/is-authenticated.ts
@@ -1,11 +1,14 @@
 import { cookies } from 'next/headers'
 
-import { ACCESS_TOKEN_EXPIRES } from '@/app/api/constants'
-
+import { ACCESS_TOKEN_EXPIRES, REFRESH_TOKEN } from '@/app/api/constants'
+import { refreshAccessToken } from '@/app/api/fetchers/refresh-token'
 export function isAuthenticated() {
   const accessTokenExpires = cookies().get(ACCESS_TOKEN_EXPIRES)?.value
+  const refreshToken = cookies().get(REFRESH_TOKEN)?.value
 
   if (accessTokenExpires === undefined) return false
 
-  return Date.now() >= new Date(accessTokenExpires).getTime() ? false : true
+  return Date.now() >= new Date(accessTokenExpires).getTime()
+    ? refreshAccessToken(refreshToken)
+    : true
 }
